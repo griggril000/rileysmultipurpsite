60690b2fd0ffb25dd74a8ba67a3e6f7af99603c7
griggril000 <griggriley@gmail.com>
2017-04-24 15:19:06 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("foot01").innerHTML =
+"<p>&copy;2016- " + new Date().getFullYear() + " Riley Grigg <br> If this page doesn't load right then refresh it and it should work.</p>";
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='home.html'>Home</a></li>" +
+"<li><a href='list of games.html'>List of Games</a></li>" +
+"<li><a href='myPage/index.html'>My Page Log In</a></li>" +
+"<li><a href='index.html'>Log out</a></li>"
+"</ul>";
