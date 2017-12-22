6603929cb3c0a369ef8b6cb1c8c8efecd23e2240
Riley Grigg <griggriley@gmail.com>
2017-12-22 14:07:00 -0700

    Create script.js

---
+++ mypage/script.js
@@ -0,0 +1,7 @@
+document.getElementById("foot01").innerHTML =
+"<p>&copy;2016- " + new Date().getFullYear() + " Riley Grigg <br> If this page doesn't load right then refresh it and it should work.</p>";
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='/rileysmultipurpsite/mypage/home'>Home</a></li>" +
+"<li><a href='../'>Sign Out</a></li>"
+"</ul>";
