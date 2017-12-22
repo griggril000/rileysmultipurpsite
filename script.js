f74892568952e091a21b79a3cc35c4ed2b6d1e91
Riley Grigg <griggriley@gmail.com>
2017-12-22 13:43:21 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById("foot01").innerHTML =
 "<p>&copy;2016- " + new Date().getFullYear() + " Riley Grigg <br> If this page doesn't load right then refresh it and it should work.</p>";
 document.getElementById("nav01").innerHTML =
 "<ul id='menu'>" +
-"<li><a href='/'>Home</a></li>" +
+"<li><a href='/rileysmultipurpsite'>Home</a></li>" +
 "<li><a href='games'>List of Games</a></li>" +
 "<li><a href='mypage/index'>My Page Log In</a></li>"
 "</ul>";
